a0017286b4b6f6515c44c3f24e155c5e210e8b30
AuTong <D1093068@o365.fcu.edu.tw>
2023-05-08 01:34:30 +0800

    modify python

--- python/connect.py
+++ python/connect.py
@@ -15,36 +15,86 @@ try:
     with conn.cursor() as cursor:
 
         stdid = "D1060001" #前端傳的值
-
+        
+        sql_std = 'SELECT * FROM db_students WHERE `"學生學號"` = %s'
+        params_std = (stdid)
+        cursor.execute(sql_std,params_std)
+        stduent = []
+        for row in cursor:
+            line = []
+            for i in row:
+                line.append(i)
+            stduent.append(line)
+        print(stduent)
+        
     # 已選課表
-
         # 查詢語句，參數化防止sql注入 
-        sql_selected = 'SELECT * FROM db_course where db_course.`"選課代號"` IN (SELECT `"已選課程代碼"` FROM db_courseselected where `"學生學號"` = %s GROUP by `"已選課程代碼"`) GROUP by `"選課代號"`'
+        sql_selected = 'SELECT * FROM db_course LEFT JOIN db_coursetime ON db_course.`"選課代號"`= db_coursetime.`"選課代號"` where db_course.`"選課代號"` IN (SELECT `"已選課程代碼"` FROM db_courseselected where `"學生學號"` = %s GROUP by `"已選課程代碼"`) GROUP by db_course.`"選課代號"`'
         params_selected = (stdid)
-
         cursor.execute(sql_selected,params_selected)
-        selected = cursor.fetchall() #type(result) = tuple
+
+        selected = []
+        for row in cursor:
+            line = []
+            for i in row:
+                line.append(i)
+            selected.append(line)
         print(selected)
         
-        '''以下是半成品
-        sql_selectedID_from_stdid = 'SELECT `"已選課程代碼"` FROM `db_courseselected` WHERE `"學生學號"`= %s'
-        params = (stdid)
-
-        cursor.execute(sql_selectedID_from_stdid,params)
-        selected_ID = cursor.fetchall() #type(result) = tuple
-        for i in selected_ID:
-            sql_course_from_courseID ='SELECT * from `db_course` where `"課程" sqli[0]
-        print()
-        '''
-
     # 可選課表
-        sql_selectable = 'SELECT * from db_course where `db_course`.`"選課代號"` NOT IN (SELECT `"選課代號"` from db_course WHERE db_course.`"開課科系"` IN (SELECT SUBSTR(`"學生班級"`, 1,2) as class FROM db_students where `"學生學號"` = %s GROUP by class) and db_course.`"開課年級"` IN (SELECT SUBSTR(`"學生班級"`, 3,1) as grade FROM db_students where `"學生學號"` = %s GROUP by grade) and db_course.`"必選修"` = "M" GROUP BY db_course.`"選課代號"`) GROUP BY db_course.`"選課代號"`;'
+        sql_selectable = 'SELECT * from db_course LEFT JOIN db_coursetime ON `db_course`.`"選課代號"`= `db_coursetime`.`"選課代號"` where `db_course`.`"選課代號"` NOT IN (SELECT `"選課代號"` from db_course WHERE db_course.`"開課科系"` IN (SELECT SUBSTR(`"學生班級"`, 1,2) as class FROM db_students where `"學生學號"` = %s GROUP by class) and db_course.`"開課年級"` IN (SELECT SUBSTR(`"學生班級"`, 3,1) as grade FROM db_students where `"學生學號"` = %s GROUP by grade) and db_course.`"必選修"` = "M" GROUP BY db_course.`"選課代號"`) GROUP BY db_course.`"選課代號"`;'
         params_selectable = (stdid,stdid)
         cursor.execute(sql_selectable,params_selectable)
-        selectable = cursor.fetchall() #type(result) = tuple
+
+        selectable = []
+        for row in cursor:
+            line = []
+            for i in row:
+                line.append(i)
+            selectable.append(line)
         print(selectable)
-        
+
+    # 判斷
+        flag = 0 # 0:可選 1:不可選 2:已選
+
+        for i in selectable:
+            # 判斷是否已選
+            flag = 0
+            for j in selected:
+                if i[0] == j[0]:
+                    flag = 2
+                    break
+            # 判斷是否已選滿 
+            if i[9] >= i[8] and flag != 2:
+                flag = 1
+
+
+            # 判斷是否衝堂
+            for j in selected:
+                # i 要選的課程 j 已選的課程
+                # 13:星期 14:開始時間 15:結束時間
+                if ((i[13] == j[13]) and ((int(i[14]) >= int(j[14]) and int(i[14]) <= int(j[15])) or (int(i[15]) >= int(j[14]) or int(i[15]) <= int(j[15])) or (int(i[14]) <= int(j[14]) and int(i[15]) >= int(j[15])))) and flag != 2:
+                    print(i[13],j[13],i[14],i[15],j[14],j[15])
+                    flag = 3
+                    break
+
+            # 判斷是否超過學分
+            if int(i[5]) + int(stduent[0][3]) > 30 and flag != 2:
+                flag = 4
          
+            print(flag)
+        #    i.append(flag) 
+
+       # print(selectable)
+
+
+        
+
+
+        for i in selectable:
+            i.append(flag)
+
+        #print(selectable)
         
 except Exception as ex:
     print(ex)
